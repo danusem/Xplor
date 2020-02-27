22c99acd32a45217bbba77791d94c531fcb03e5d
Dan Usem <danusem@gmail.com>
2020-02-27 14:53:32 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home/Home';
+import Destinations from './pages/Destinations/Destinations';
+
 import './App.css';
 
 function App() {
@@ -7,8 +14,16 @@ function App() {
     <div className="App-outer-container">
        <Navbar />
        <div className="App-inner-container">
-
+          <Switch>
+            <Route exact path="/" render={props =>
+              <Home />
+            }/>
+             <Route exact path="/destinations" render={props =>
+              <Destinations />
+            }/>
+          </Switch>
        </div>
+       <Footer /> 
     </div>
   );
 }
