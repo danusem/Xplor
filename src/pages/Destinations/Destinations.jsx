c0720a8d24a883b943dcac468f48a5c619b2013c
Dan Usem <danusem@gmail.com>
2020-02-27 14:52:13 -0600

    added Destinations component

---
+++ src/pages/Destinations/Destinations.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Destinations.module.css';
+
+const Destinations = (props) => {
+    return (
+        <main>
+            <h1>Destinations</h1>
+        </main>
+    );
+};
+
+export default Destinations;
