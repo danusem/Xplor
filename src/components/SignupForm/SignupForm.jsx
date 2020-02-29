961e0f687630783e61df73a32021445f241df4e3
Dan Usem <danusem@gmail.com>
2020-02-29 09:04:10 -0700

    updated signup form

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class SignupForm extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        passwordConf: ''
+    };
+
+    render () {
+        return (
+            <form>
+                <fieldset>
+                    <legend>Signup Form</legend>
+                    <label htmlFor="name">Full Name</label>
+                    <input id="name" name="name" type="text" value={this.state.name}/>
+
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="email" value={this.state.email}/>
+
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" value={this.state.password}/>
+
+                    <label htmlFor="passwordConf">Password Confirmation</label>
+                    <input id="passwordConf" name="passwordConf" type="password" value={this.state.passwordConf}/>
+
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
+        );
+    }
+}
+
+export default SignupForm;
