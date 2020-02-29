2b08e15827722212d3c8bfe36c556df924f90a93
Dan Usem <danusem@gmail.com>
2020-02-29 09:48:04 -0700

    addded event handlers to signup form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -9,22 +9,52 @@ class SignupForm extends Component {
         passwordConf: ''
     };
 
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     render () {
         return (
             <form>
                 <fieldset>
                     <legend>Signup Form</legend>
                     <label htmlFor="name">Full Name</label>
-                    <input id="name" name="name" type="text" value={this.state.name}/>
+                    <input 
+                        id="name" 
+                        name="name" 
+                        type="text" 
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
 
                     <label htmlFor="email">Email</label>
-                    <input id="email" name="email" type="email" value={this.state.email}/>
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
 
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password" value={this.state.password}/>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
 
                     <label htmlFor="passwordConf">Password Confirmation</label>
-                    <input id="passwordConf" name="passwordConf" type="password" value={this.state.passwordConf}/>
+                    <input 
+                        id="passwordConf" 
+                        name="passwordConf" 
+                        type="password" 
+                        value={this.state.passwordConf}
+                        onChange={this.handleChange}
+                    />
 
                     <button type="submit">Submit</button>
                 </fieldset>
