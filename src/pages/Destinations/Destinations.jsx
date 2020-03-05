d4c7ece91080b7c5cfd18c99ab99d952fa84a589
Dan Usem <danusem@gmail.com>
2020-03-04 20:58:32 -0600

    refactors destination page

--- src/pages/Destinations/Destinations.jsx
+++ src/pages/Destinations/Destinations.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import styles from './Destinations.module.css';
+// import styles from './Destinations.module.css';
+
+import DestinationForm from '../../components/DestinationForm/DestinationForm';
 
 const Destinations = (props) => {
     return (
         <main>
             <h1>Destinations</h1>
+            <DestinationForm {...props} />
         </main>
     );
 };
