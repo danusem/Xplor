f3c190ed6ecae29d988ac93f5e7559cc228dc4e8
Dan Usem <danusem@gmail.com>
2020-03-06 12:09:10 -0600

    updated css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,15 @@ import styles from './Home.module.css';
 const Home = (props) => {
     return (
         <main className={styles.home}>
+            <h1>Featured Destination</h1>
+            {
+                props.featuredDestinations.map(({city, _id}) => (
+                    <section key={_id}>
+                        <h2>{city}</h2>
+                    </section>
+
+                ))
+            }
             <img className={styles.pic} src={island} alt="island"/>
         </main>
     );
