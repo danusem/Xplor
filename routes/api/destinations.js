75cb07233e37bfaff2a5729b62c9a44ae0ad2b1b
Dan Usem <danusem@gmail.com>
2020-03-06 21:51:33 -0600

    adds authorization

--- routes/api/destinations.js
+++ routes/api/destinations.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const destinationCtrl = require('../../controllers/destinations');
 
-router.post('/', destinationCtrl.create);
-router.get('/', destinationCtrl.index);
+
 router.get('/featured', destinationCtrl.getFeatured);
+
+router.use(require('../../config/auth'));
+
+router.post('/', isAuthenticated, destinationCtrl.create);
+router.get('/', isAuthenticated, destinationCtrl.index);
+
+function isAuthenticated(req, res, next) {
+    if(req.user) return next();
+    return res.status(401).json({msg: 'not authorized'})
+}
 
 module.exports = router;
